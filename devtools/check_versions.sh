b17174625141bfaad89212dc95b970374e4191c9
vrtulka23 <vrtulka23@pm.me>
2026-06-25 12:00:17 +0200

    added conda recipes and increased the versions

--- devtools/check_versions.sh
+++ devtools/check_versions.sh
@@ -8,5 +8,6 @@ DIR_SOURCE=.
 
 echo "pyproject.toml              " $(grep "^version =" $DIR_SOURCE/pyproject.toml)
 echo "settings.env                " $(grep CODE_VERSION $DIR_SOURCE/settings.env)
+echo "conda-recipe/meta.yaml      " $(grep "version =" $DIR_SOURCE/conda-recipe/meta.yaml)
 echo "docs/dipl/specification.md  " $(grep "Version:" $DIR_SOURCE/docs/dipl/specification.md) 
 echo "docs/puel/specification.md  " $(grep "Version:" $DIR_SOURCE/docs/puel/specification.md) 
