78391eb79020f7aa2c82f14d3e0604c8b517d7b3
vrtulka23 <vrtulka23@pm.me>
2026-08-08 23:58:55 +0200

    implemented additional argument testing in pybind api tests

--- bindings/python/tests/api/test_api_dip.py
+++ bindings/python/tests/api/test_api_dip.py
@@ -1,20 +1,73 @@
 import pytest
+import tempfile
+import os
 
 from scinumtools3.api.dip import DIPParse
 
-def test_parse():
+def test_add_string():
 
     c = DIPParse()
-
-    c.argument_add("string",["foo int = 32 cm"])
-    c.argument_add("string",["bar float = 13.3e4 kg"])
-    c.argument_add("string",["baz str = \"snap\""])
+    c.argument_add("string",[
+        "foo int = 32 cm\n"
+        "bar float = 13.3e4 kg\n"
+        "baz str = \"snap\""
+    ])
     c.argument_print()
     
-    # TODO: test all arguments
-
     assert c.execute() == (
         "foo = 32 cm\n"
         "bar = 1.33e5 kg\n"
         "baz = \"snap\"\n"
     )
+
+def test_add_file():
+
+    with tempfile.NamedTemporaryFile(mode="w", suffix=".dip", delete=False) as f:
+        f.write(
+            "foo int = 32 cm\n"
+            "bar float = 13.3e4 kg\n"
+            "baz str = \"snap\""
+        )
+        filename = f.name
+        
+    c = DIPParse()
+    c.argument_add("file",[filename])
+    c.argument_print()
+    
+    assert c.execute() == (
+        "foo = 32 cm\n"
+        "bar = 1.33e5 kg\n"
+        "baz = \"snap\"\n"
+    )
+
+    os.unlink(filename)
+
+def test_request():
+
+    c = DIPParse()
+    c.argument_add("string",[
+        "foo int = 32 cm\n"
+        "bar float = 13.3e4 kg"
+    ])
+    c.argument_request("bar")
+    c.argument_print()
+    
+    assert c.execute() == (
+        "bar = 1.33e5 kg\n"
+    )
+
+def test_tags():
+
+    c = DIPParse()
+    c.argument_add("string",[
+        "foo int = 32 cm\n"
+        "bar float = 13.3e4 kg\n"
+        "  !tags [\"baz\"]"
+    ])
+    c.argument_tags(["baz"])
+    c.argument_print()
+    
+    assert c.execute() == (
+        "bar = 1.33e5 kg\n"
+    )
+    
--- bindings/python/tests/api/test_api_puq.py
+++ bindings/python/tests/api/test_api_puq.py
@@ -6,30 +6,46 @@ def test_convert():
 
     c = PUQConvert("cm", "km")
     
-    # TODO: test arguments
-
     assert c.execute() == "1e-5*km"
 
+    c = PUQConvert("15*ft", "m")
+    c.argument_input_system("US")
+    c.argument_output_system("SI")
+    c.argument_output_quantity("l")
+
+    assert c.execute() == "4.572*m"
+    
 def test_eval():
 
     c = PUQEval("1*cm + 2*dm")
 
-    # TODO: test arguments
-
     assert c.execute() == "21*cm"
 
+    c.argument_input_system("SI")
+    c.argument_output_system("US")
+    c.argument_output_units("ft")
+    c.argument_output_quantity("l")
+
+    assert c.execute() == "0.688976*ft"
+    
 def test_info():
 
     c = PUQInfo("J")
 
-    # TODO: test arguments
-    
     assert c.execute() == """\nExpression:  J\n\nUnit system: SI (International System of Units)\nResult:   1\nBase units:  J\n\nQuantities:  E, Q, W, tau\n\nDimensions:\n\nBase   Num*Mag                   Numerical                 Physical                  \n------ ------------------------- ------------------------- ------------------------- \nMGS    1e3                       1e3                       m2*g*s-2                  \nMKS    1                         1                         m2*kg*s-2                 \nCGS    1e7                       1e7                       cm2*g*s-2                 \n\nBase units:\n\nPrefix   Symbol   Exponent   Name                           Definition                     Dimensions MGS         Allowed prefixes       \n-------- -------- ---------- ------------------------------ ------------------------------ ---------------------- ---------------------- \n         J        1          Joule                          N*m                            1e3*m2*g*s-2           all                    \n"""
 
+    c = PUQInfo("ft2*lb")
+    c.argument_input_system("US")
+
+    assert c.execute() == """\nExpression:  ft2*lb\n\nUnit system: US (United States customary units)\nResult:   1\nBase units:  ft2*lb\n\nDimensions:\n\nBase   Num*Mag                   Numerical                 Physical                  \n------ ------------------------- ------------------------- ------------------------- \nMGS    42.1401                   42.1401                   m2*g                      \nMKS    0.0421401                 0.0421401                 m2*kg                     \nCGS    4.21401e5                 4.21401e5                 cm2*g                     \n\nBase units:\n\nPrefix   Symbol   Exponent   Name                           Definition                     Dimensions MGS         Allowed prefixes       \n-------- -------- ---------- ------------------------------ ------------------------------ ---------------------- ---------------------- \n         ft       2          foot                           0.3048*m                       0.092903*m2                                   \n         lb       1          pound                          453.59237*g                    453.592*g                                     \n"""
+    
 def test_list():
 
     c = PUQList("AU")
-
-    # TODO: test arguments
     
     assert c.execute() == """\nList 'AU' does not exist.\n\nAvailable lists:\n\nprefix  unit prefixes\nbase    base units\nderiv   derived units\nlog     logarithmic units\ntemp    temperature units\nconst   constants\nquant   quantities\nsys     unit systems\n"""
+
+    c = PUQList("deriv")
+    c.argument_system("AU")
+
+    assert c.execute() == """Symbol    Name                   Result        Dimension                      Definition                Allowed prefixes       \n--------- ---------------------- ------------- ------------------------------ ------------------------- ---------------------- \nE_h       hartree                4.3597447276(40)e-15 m2*g*s-2                       {#hbar}2/({#m_e}*{a_0}2)                         \n"""
